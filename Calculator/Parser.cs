d2df02bc0bce3ee4f0e06235137acfdf921a7071
kgazha <kgazha@gmail.com>
2021-01-23 13:41:54 +0500

    Add expression parsing

--- Calculator/Parser.cs
+++ Calculator/Parser.cs
@@ -11,7 +11,13 @@ namespace Calculator
         public Parser()
         {
             patterns = new Dictionary<string, Token>();
-            patterns.Add(@"", Token.Number);
+            patterns.Add(@"^-?\d+(\.\d+)?", Token.Number);
+            patterns.Add(@"^\(", Token.LeftBracket);
+            patterns.Add(@"^\)", Token.RightBracket);
+            patterns.Add(@"^\+", Token.Addition);
+            patterns.Add(@"^\-", Token.Subtraction);
+            patterns.Add(@"^\*", Token.Multiplication);
+            patterns.Add(@"^\/", Token.Division);
         }
 
         public string PrepareExpression(string expression)
@@ -25,7 +31,23 @@ namespace Calculator
             List<Symbol> symbols = new List<Symbol>();
             while (expression != "")
             {
-                
+                bool patternFound = false;
+                foreach (var pattern in patterns)
+                {
+                    var result = Regex.Match(expression, pattern.Key);
+                    if (result.Success)
+                    {
+                        Symbol symbol = new Symbol { Value = result.Value, Token = pattern.Value };
+                        symbols.Add(symbol);
+                        expression = expression.Substring(result.Value.Length);
+                        patternFound = true;
+                        break;
+                    }
+                }
+                if (!patternFound)
+                {
+                    throw new Exception("Failed to parse input expression! " + expression);
+                }
             }
             return symbols;
         }
---
+++ CalculatorTests/ParserTest.cs
@@ -0,0 +1,40 @@
+using Calculator;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace CalculatorTests
+{
+    public class ParserTest
+    {
+        Parser parser = new Parser();
+
+        [Fact]
+        public void PrepareExpressionTest()
+        {
+            string expression = "3 +  4*4-( -3.4)";
+            var actualResult = parser.PrepareExpression(expression);
+            var expectedResult = "3+4*4-(-3.4)";
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void PraseTest()
+        {
+            string expression = "34.5 + 26.5*-2";
+            var actualResult = parser.Parse(expression);
+            var expectedResult = new List<Symbol>() { 
+                new Symbol() { Token=Token.Number, Value="34.5" },
+                new Symbol() { Token=Token.Addition, Value="+" },
+                new Symbol() { Token=Token.Number, Value="26.5" },
+                new Symbol() { Token=Token.Multiplication, Value="*" },
+                new Symbol() { Token=Token.Number, Value="-2" },
+            };
+            for (int i = 0; i < actualResult.Count; i++)
+            {
+                Assert.Equal(expectedResult[i].Token, actualResult[i].Token);
+                Assert.Equal(expectedResult[i].Value, actualResult[i].Value);
+            }
+        }
+    }
+}
